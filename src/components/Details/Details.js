d150b70d500d4c424de777b43cc30892f72d063e
benardop <benard.oc.p@gmail.com>
2022-10-12 22:21:43 +0300

    Update Details script file

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import style from './Details.module.css';
 import { deleteDetails } from '../../redux/detailsReducer/detailsReducer';
+import arrowImg from '../../assets/images/arrow.svg';
 
 function Details() {
   const details = useSelector((state) => state.details);
@@ -11,50 +12,57 @@ function Details() {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }, [dispatch]);
   return (
-    <div className={style.container}>
-      <img src={details.flag} alt="Flag" />
-      <h2>{details.name}</h2>
-      <div>
-        <span>Capitel</span>
-        {' '}
-        <span>:</span>
-        {' '}
-        <span>{details.name}</span>
+    <div className={style.container} data-testid="details-container">
+      <div className={style.top}>
+        <img src={details.flag} alt="Flag" />
+        <div className={style.name}>
+          <h2>{details.name}</h2>
+          <span>{details.timezone}</span>
+        </div>
+
       </div>
-      <div>
+
+      <div className={style.detail}>
+        <span>Capital</span>
+        <div>
+          <span>{details.capital}</span>
+          <img src={arrowImg} alt="arrow" />
+        </div>
+      </div>
+      <div className={style.detail}>
         <span>Timezone</span>
-        {' '}
-        <span>:</span>
-        {' '}
-        <span>{details.timezone}</span>
+        <div>
+          <span>{details.timezone}</span>
+          <img src={arrowImg} alt="arrow" />
+        </div>
       </div>
-      <div>
+      <div className={style.detail}>
         <span>population</span>
-        {' '}
-        <span>:</span>
-        {' '}
-        <span>{details.population}</span>
+        <div>
+          <span>{details.population}</span>
+          <img src={arrowImg} alt="arrow" />
+        </div>
       </div>
-      <div>
+      <div className={style.detail}>
         <span>Formal Language</span>
-        {' '}
-        <span>:</span>
-        {' '}
-        <span>{details.firstLanguage}</span>
+        <div>
+          <span>{details.firstLanguage}</span>
+          <img src={arrowImg} alt="arrow" />
+        </div>
       </div>
-      <div>
+      <div className={style.detail}>
         <span>Area</span>
-        {' '}
-        <span>:</span>
-        {' '}
-        <span>{details.area}</span>
+        <div>
+          <span>{details.area}</span>
+          <img src={arrowImg} alt="arrow" />
+        </div>
       </div>
-      <div>
+      <div className={style.detail}>
         <span>Currency</span>
-        {' '}
-        <span>:</span>
-        {' '}
-        <span>{details.currencies}</span>
+        <div>
+          <span>{details.currencies}</span>
+          <img src={arrowImg} alt="arrow" />
+        </div>
       </div>
     </div>
   );
