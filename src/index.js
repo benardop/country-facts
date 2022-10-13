6fcc62afa6f3149ee66e9d2292a497c745560c99
benardop <benard.oc.p@gmail.com>
2022-10-13 19:49:57 +0300

    Update index.js with router details

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <React.StrictMode>
         <App />
@@ -16,6 +16,6 @@ root.render(
       ,
     </Provider>
     ,
-  </BrowserRouter>,
+  </HashRouter>,
 
 );
